2e35c7496453abae28f2d36002fcbe23399c2d23
jenybo <86452879+JenyBo@users.noreply.github.com>
2023-07-06 19:29:24 +0700

    services_update

--- src/services/GoiTapServices.java
+++ src/services/GoiTapServices.java
@@ -1,7 +1,63 @@
 package services;
 
-public class GoiTapServices {
+import entity.db.GymDB;
+import entity.model.GoiTap;
+import entity.model.HoiVien;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static utils.Utils.convertDate;
+
+public class GoiTapServices {
+	public static ObservableList<GoiTap> getAllHoiVien() throws SQLException {
+
+		ObservableList<GoiTap> goiTapList = FXCollections.observableArrayList();
+		String SELECT_QUERY = "SELECT * FROM `goi_tap`";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(SELECT_QUERY);
+		ResultSet result = preparedStatement.executeQuery();
+
+		while (result.next()) {
+			goiTapList.add(new GoiTap(result.getInt("id"), result.getInt("so_tien"),
+					result.getString("ten_goi_tap"), result.getString("loai_goi_tap")));
+		}
+
+		return goiTapList;
+	}
+
+	public static int addGoiTap(int soTien, String tenGoiTap, String loaiGoiTap) throws SQLException {
+
+		String INSERT_QUERY = "INSERT INTO `goi_tap`(`ten_goi_tap`, `so_tien`, `loai_goi_tap`) VALUES (?, ?, ?)";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(INSERT_QUERY);
+		preparedStatement.setString(1, tenGoiTap);
+		preparedStatement.setInt(2, soTien);
+		preparedStatement.setString(3, loaiGoiTap);
+
+		return preparedStatement.executeUpdate();
+	}
+
+	public static int deleteGoiTap(int ID) throws SQLException {
+		String DELETE_QUERY =
+				"DELETE FROM `goi_tap` " +
+						"WHERE `id` =?";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(DELETE_QUERY);
+		preparedStatement.setInt(1, ID);
+		return preparedStatement.executeUpdate();
+	}
+
+	public static int updateHoiVien(int ID, int soTien, String tenGoiTap, String loaiGoiTap) throws SQLException {
+		String UPDATE_QUERY = "UPDATE `hoi_vien` SET `ten_goi_tap`=?, `so_tien`=?, `loai_goi_tap`=? WHERE `id` = ?";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(UPDATE_QUERY);
+		preparedStatement.setString(1, tenGoiTap);
+		preparedStatement.setInt(2, soTien);
+		preparedStatement.setString(3, loaiGoiTap);
+		preparedStatement.setInt(4, ID);
+		preparedStatement.execute();
+		return preparedStatement.executeUpdate();
+	}
 	public GoiTapServices() {
 		// TODO Auto-generated constructor stub
 	}
--- src/services/ThietBiServices.java
+++ src/services/ThietBiServices.java
@@ -1,7 +1,94 @@
 package services;
 
-public class ThietBiServices {
+import entity.db.GymDB;
+import entity.model.GoiTap;
+import entity.model.ThietBi;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static utils.Utils.convertDate;
+
+public class ThietBiServices {
+	public static ObservableList<ThietBi> getAllHoiVien() throws SQLException {
+
+		ObservableList<ThietBi> thietBiList = FXCollections.observableArrayList();
+		String SELECT_QUERY = "SELECT * FROM `thiet_bi`";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(SELECT_QUERY);
+		ResultSet result = preparedStatement.executeQuery();
+
+		while (result.next()) {
+			thietBiList.add(new ThietBi(result.getInt("id"), result.getInt("id_phong_tap"),
+					result.getString("ten"), convertDate(result.getString("ngay_nhap_ve")),
+					result.getString("xuat_xu"),result.getString("tinh_trang")));
+		}
+
+		return thietBiList;
+	}
+
+	public static int addThietBi(int idRoom, String ten, String ngayNhapVe, String xuatXu, String tinhTrang) throws SQLException {
+
+		String INSERT_QUERY = "INSERT INTO `thiet_bi`(`id_phong_tap`, `ten`, `ngay_nhap_ve`, " +
+				"`xuat_xu`, `tinh_trang`) VALUES (?, ?, ?, ?, ?)";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(INSERT_QUERY);
+		preparedStatement.setInt(1, idRoom);
+		preparedStatement.setString(2, ten);
+		preparedStatement.setString(3, ngayNhapVe);
+		preparedStatement.setString(4, xuatXu);
+		preparedStatement.setString(5, tinhTrang);
+
+		return preparedStatement.executeUpdate();
+	}
+
+	public static ThietBi findThietBiById(int ID) throws SQLException{
+		String SELECT_QUERY = "SELECT * FROM `thiet_bi` WHERE `id` = ?";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(SELECT_QUERY);
+		ResultSet result = preparedStatement.executeQuery();
+		ThietBi thietBi = new ThietBi(result.getInt("id"), result.getInt("id_phong_tap"),
+				result.getString("ten"), convertDate(result.getString("ngay_nhap_ve")),
+				result.getString("xuat_xu"),result.getString("tinh_trang"));
+
+		return thietBi;
+	}
+
+	public static ObservableList<ThietBi> findThietBiByIdRoom(int idRoom) throws SQLException {
+		ObservableList<ThietBi> thietBiList = FXCollections.observableArrayList();
+		String SELECT_QUERY = "SELECT * FROM `thiet_bi` WHERE `id_phong_tap` = ?";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(SELECT_QUERY);
+		ResultSet result = preparedStatement.executeQuery();
+		while (result.next()) {
+			thietBiList.add(new ThietBi(result.getInt("id"), result.getInt("id_phong_tap"),
+					result.getString("ten"), convertDate(result.getString("ngay_nhap_ve")),
+					result.getString("xuat_xu"), result.getString("tinh_trang")));
+		}
+		return thietBiList;
+	}
+
+//	public static int deleteThietBi(int ID) throws SQLException {
+//		String DELETE_QUERY =
+//				"DELETE FROM `goi_tap` " +
+//						"WHERE `id` =?";
+//		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(DELETE_QUERY);
+//		preparedStatement.setInt(1, ID);
+//		return preparedStatement.executeUpdate();
+//	}
+
+	public static int updateThietBi(int ID, int idRoom, String ten, String ngayNhapVe, String xuatXu, String tinhTrang) throws SQLException {
+		String UPDATE_QUERY = "UPDATE `hoi_vien` SET `id_phong_tap`=?, `ten`=?, `ngay_nhap_ve`=?," +
+				"`xuat_xu`=?, `tinh_trang`=? WHERE `id` = ?";
+		PreparedStatement preparedStatement = GymDB.getConnection().prepareStatement(UPDATE_QUERY);
+		preparedStatement.setInt(1, idRoom);
+		preparedStatement.setString(2, ten);
+		preparedStatement.setString(3, ngayNhapVe);
+		preparedStatement.setString(4, xuatXu);
+		preparedStatement.setString(5, tinhTrang);
+		preparedStatement.setInt(6, ID);
+		preparedStatement.execute();
+		return preparedStatement.executeUpdate();
+	}
 	public ThietBiServices() {
 		// TODO Auto-generated constructor stub
 	}
